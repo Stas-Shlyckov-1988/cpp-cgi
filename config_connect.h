6461f0c05c8787deee9c0a6b4f38b1f842626670
Stas-Shlyckov-1988 <stas.shlyckov@yandex.ru>
2024-10-02 07:43:07 +0300

    format code

--- config_connect.h
+++ config_connect.h
@@ -2,8 +2,8 @@
 #define CONFIG_CONNECT_H
 struct connection
 {
-    unsigned port = 3306;
-    const char *host = "localhost";
+	unsigned port = 3306;
+	const char *host = "localhost";
 	const char *user = "mpeegy";
 	const char *password = "procedure";
 	const char *dbname = "test";
