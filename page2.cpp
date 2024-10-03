fb2977d3d5bd800ef875cb25fe754bf146b46e14
Stas-Shlyckov-1988 <stas.shlyckov@yandex.ru>
2024-10-03 09:58:51 +0300

    add page

---
+++ page2.cpp
@@ -0,0 +1,63 @@
+//  g++ page2.cpp -o page2.cgi -L/usr/include/mysql -lmysqlclient -lcgicc
+#include <iostream>
+#include <mysql/mysql.h>
+#include <cgicc/Cgicc.h>
+#include <string>
+#include "config_connect.h"
+
+using namespace std;
+using namespace cgicc;
+
+
+int main() {
+    MYSQL *db, *conn;
+    MYSQL_RES *res_set;
+    MYSQL_ROW row;
+
+    db = mysql_init(NULL);
+    if (!db) return 1;
+	
+    struct connection c1;
+
+    conn = mysql_real_connect(db, c1.host, c1.user, c1.password, c1.dbname, c1.port, NULL, 0);
+    if (!conn) {
+        fprintf(stderr, "Failed to connect to database: Error: %s\n", mysql_error(db));
+        mysql_close(conn);
+        return 2;
+    }
+
+    cout << "Content-Type: text/html" << endl << endl;
+    cout << "<link rel=\"stylesheet\" href=\"/main.css\" />" << endl;
+    cout << "<h1>Staff</h1>" << endl;
+
+    Cgicc cgi;
+    string id = cgi("id");
+    //mysql_real_escape_string(conn, id, id.c_str(), strlen(id));
+    string query = "SELECT * FROM Staff WHERE id = " + id;
+
+    mysql_query(conn, query.c_str());
+    res_set = mysql_store_result(conn); 
+    
+    MYSQL_FIELD *mfield;
+    unsigned int num_fields;
+    unsigned int i;
+
+    num_fields = mysql_num_fields(res_set);
+
+    cout << "<p>" << endl;
+    while ((row = mysql_fetch_row(res_set)))
+    {
+        unsigned long *lengths;
+        lengths = mysql_fetch_lengths(res_set);
+        for(i = 0; i < num_fields; i++)
+        {
+            printf("[%.*s] ", (int) lengths[i],
+                    row[i] ? row[i] : "NULL");
+        }
+        printf("<br>");
+    }
+    cout << "</p>" << endl;
+    mysql_free_result(res_set);
+    mysql_close(conn);
+    return 0;
+}
