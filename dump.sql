879fc0187b2ba11f2d80d5e4e18d8142b54475c9
Stas-Shlyckov-1988 <stas.shlyckov@yandex.ru>
2024-10-06 13:45:13 +0300

    damp sql

--- dump.sql
+++ dump.sql
@@ -1,30 +1,45 @@
-CREATE DATABASE `test`;
+-- --------------------------------------------------------
+-- Хост:                         127.0.0.1
+-- Версия сервера:               8.0.39-0ubuntu0.22.04.1 - (Ubuntu)
+-- Операционная система:         Linux
+-- HeidiSQL Версия:              12.8.0.6908
+-- --------------------------------------------------------
 
--- test.Staff definition
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE `Staff` (
-  `id` int DEFAULT NULL,
-  `name` varchar(255) NOT NULL,
-  `position` varchar(30) DEFAULT NULL,
-  `birthday` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO test.Staff
-(id, name, `position`, birthday)
-VALUES(1, 'test1', 1, '2024-09-29');
+-- Дамп структуры базы данных test
+DROP DATABASE IF EXISTS `test`;
+CREATE DATABASE IF NOT EXISTS `test` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `test`;
 
-INSERT INTO test.Staff
-(id, name, `position`, birthday)
-VALUES(2, 'test2', 2, '2024-09-29');
+-- Дамп структуры для таблица test.Staff
+DROP TABLE IF EXISTS `Staff`;
+CREATE TABLE IF NOT EXISTS `Staff` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL DEFAULT '0',
+  `position` int NOT NULL DEFAULT (0),
+  `birthday` varchar(50) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO test.Staff
-(id, name, `position`, birthday)
-VALUES(3, 'test3', 3, '2024-09-30');
+-- Дамп данных таблицы test.Staff: ~5 rows (приблизительно)
+INSERT INTO `Staff` (`id`, `name`, `position`, `birthday`) VALUES
+	(1, 'test', 5, '2010-10-12'),
+	(2, 'test2', 1, '2000-02-20'),
+	(3, 'test3', 4, '2000-02-20'),
+	(4, 'test4', 2, '2010-10-12'),
+	(5, 'test5', 3, '2010-10-12');
 
-INSERT INTO test.Staff
-(id, name, `position`, birthday)
-VALUES(4, 'test4', 4, '2024-09-30');
-
-INSERT INTO test.Staff
-(id, name, `position`, birthday)
-VALUES(5, 'test5', 5, '2024-09-30');
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
